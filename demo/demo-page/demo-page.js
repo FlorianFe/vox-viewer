f420a7319905c18a7f38a7f4e95e379030af6bfe
FlorianFe <webtutorialdesign@gmail.com>
2022-06-01 20:05:38 +0200

    update deps

--- demo/demo-page/demo-page.js
+++ demo/demo-page/demo-page.js
@@ -1,77 +1,80 @@
+import "../../vox-viewer";
+import { LitElement, html } from "lit-element";
 
-import '../../vox-viewer'
-import { LitElement, html } from 'lit-element'
+class DemoPage extends LitElement {
+  static get is() {
+    return "demo-page";
+  }
 
-class DemoPage extends LitElement
-{
-    static get is()
-    {
-        return 'demo-page'
-    }
+  static get properties() {
+    return {
+      selectedModel: { type: String },
+    };
+  }
 
-    static get properties() 
-    {
-        return { 
-            selectedModel: { type: String }
+  constructor() {
+    super();
+    this.selectedModel = "deer";
+  }
+
+  onModelSelected() {
+    const modelSelection = this.shadowRoot.querySelector("#model-selection");
+
+    this.selectedModel = modelSelection.value;
+  }
+
+  render() {
+    return html`
+      <style>
+        :host {
+          display: block;
+          margin: 0px;
+          padding: 10px;
         }
-    }
 
-    constructor()
-    {
-        super()
-        this.selectedModel = 'deer'
-    }
+        .controls {
+          position: absolute;
+          top: 0;
+          left: 0;
+          padding: 15px;
+        }
 
-    onModelSelected(e)
-    {
-        const modelSelection = this.shadowRoot.querySelector("#model-selection")
+        .container {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+        }
 
-        console.log(modelSelection.value)
-        this.selectedModel = modelSelection.value
-    }
+        .vox-viewer {
+          height: calc(100vh - 50px);
+          width: calc(100vh - 50px);
+        }
+      </style>
 
-    render() 
-    {
-        return html`
-            <style>
-            
-                :host
-                {
-                    display: block;
-                    margin: 0px;
-                    padding: 10px;
-                }
+      <div class="controls">
+        Model: <br />
+        <select
+          id="model-selection"
+          value="${this.selectedModel}"
+          @change="${this.onModelSelected}"
+        >
+          <option value="deer">Deer</option>
+          <option value="monu7">Monument 7</option>
+          <option value="monu8">Monument 8</option>
+        </select>
+      </div>
 
-                .container
-                {
-                    display: flex;
-                    align-items: center;
-                    justify-content: center;
-                }
-
-            </style>
-
-            Model: <br>
-            
-            <select id="model-selection" value="${this.selectedModel}" @change="${this.onModelSelected}">
-                <option value="deer">Deer</option>
-                <option value="monu7">Monument 7</option>
-                <option value="monu8">Monument 8</option>
-            </select>
-
-            <div class="container">
-
-                <vox-viewer 
-                    src="./models/${this.selectedModel}.vox" 
-                    camera-controls
-                    auto-rotate
-                    shadow-intensity="0.3"
-                    style="height: calc(100vh - 50px); width: calc(100vh - 50px);"
-                ></vox-viewer>
-                
-            </div>
-        `;
-    }
+      <div class="container">
+        <vox-viewer
+          class="vox-viewer"
+          src="./models/${this.selectedModel}.vox"
+          camera-controls
+          auto-rotate
+          shadow-intensity="0.3"
+        ></vox-viewer>
+      </div>
+    `;
+  }
 }
 
 customElements.define(DemoPage.is, DemoPage);
